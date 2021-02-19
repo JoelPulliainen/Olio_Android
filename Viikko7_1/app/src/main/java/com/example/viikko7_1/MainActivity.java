377ec33984134f6729ddc1ab2b8f87f3f6722e4d
JoelPulliainen <joelpulliainen@hotmail.com>
2021-02-19 21:52:25 +0200

    Viikon 7 tehtävät

---
+++ Viikko7_1/app/src/main/java/com/example/viikko7_1/MainActivity.java
@@ -0,0 +1,54 @@
+package com.example.viikko7_1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.io.BufferedReader;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView text;
+    EditText input;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        text = (TextView) findViewById(R.id.textView);
+        input = (EditText) findViewById(R.id.InputText);
+        input.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                String InputToPrint = input.getText().toString();
+                text.setText(InputToPrint);
+            }
+
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count,
+                                          int after) {
+                // Fires right before text is changing
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                String InputToPrint = input.getText().toString();
+                text.setText(InputToPrint);
+            }
+        });
+    }
+
+
+    public void testFunction (View v) {
+
+        String InputToPrint = input.getText().toString();
+        System.out.println("Hello World!");
+        // text.setText("Hello World");
+        text.setText(InputToPrint);
+    }
+}
---
+++ Viikko7_T5/app/src/main/java/com/example/viikko7_t5/MainActivity.java
@@ -0,0 +1,80 @@
+package com.example.viikko7_t5;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class MainActivity extends AppCompatActivity {
+    Context context = null;
+
+    TextView text;
+    EditText inputFile;
+    EditText input;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        context = MainActivity.this;
+        setContentView(R.layout.activity_main);
+        text = (TextView) findViewById(R.id.textView);
+        input = (EditText) findViewById(R.id.Input);
+        inputFile = (EditText) findViewById(R.id.InputFile);
+    }
+
+
+    public void loadTo(View v) {
+
+        String InputToFile = input.getText().toString();
+        String Filename = inputFile.getText().toString();
+
+        try {
+            OutputStreamWriter outputStreamWriter = new OutputStreamWriter(context.openFileOutput(Filename, Context.MODE_PRIVATE));
+            outputStreamWriter.write(InputToFile);
+            outputStreamWriter.close();
+        }
+        catch (IOException e) {
+            Log.e("Exception", "File write failed: " + e.toString());
+        }
+        finally {
+            System.out.println("Written to file");
+    }
+
+    }
+
+    public void save(View v) {
+
+        String Filename = inputFile.getText().toString();
+
+        try {
+            InputStream ins = context.openFileInput(Filename);
+
+            BufferedReader br = new BufferedReader(new InputStreamReader(ins));
+            String text_from_file = "";
+
+            while ((text_from_file = br.readLine()) != null) {
+                text.setText(text_from_file);
+            }
+
+            ins.close();
+        } catch (FileNotFoundException e) {
+            Log.e("FileNotFoundException", "File not found: " + e.toString());
+        } catch (IOException e) {
+            Log.e("IOException", "Invalid input");
+        } finally {
+            System.out.println("Read from file successfully");
+        }
+    }
+
+}
